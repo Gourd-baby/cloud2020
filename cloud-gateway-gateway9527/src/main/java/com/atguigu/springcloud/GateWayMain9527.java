bc855961e4fce89712507c5b18a6332a710d2445
Gourd-baby <2932061203@qq.com>
2023-04-26 20:32:17 +0800

    getway测试

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @version 1.0
+ * @Author 作者名
+ * @Date 2023/4/26 19:34
+ * @注释
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,38 @@
+package com.atguigu.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @version 1.0
+ * @Author 作者名
+ * @Date 2023/4/26 20:23
+ * @注释
+ */
+@Component
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            System.out.println("****用户名为null，无法登录");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 定义该fiter过滤器的优先级，数字越小，优先级越大
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
